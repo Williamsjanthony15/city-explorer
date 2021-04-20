8729d7a83bf934cd8ca886d83a398b8c7bc39c48
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-04-20 18:50:22 -0500

    rendering a map with location on user input

--- src/Search.js
+++ src/Search.js
@@ -32,30 +32,37 @@ class Search extends React.Component {
     // });
     // this.props.getWeather(weatherData.data)
   }
-
   getLatLong = async (city) => {
+    // let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`;
+    // console.log(url);
+
     const locationURL = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`);
-    console.log('this is our location url!', locationURL);
+    // console.log('this is our location url!', locationURL);
     this.setState({
       cityName: locationURL.data[0].display_name,
       lat: locationURL.data[0].lat,
       lon: locationURL.data[0].lon,
 
     })
-    console.log(this.state.cityName, this.state.lat, this.state.lon);
+    // console.log(this.state.cityName, this.state.lat, this.state.lon);
   }
 
   render() {
     // console.log('this is a city!', this.citySearch);
     return (
-      <Jumbotron>
+      <>
+      <h1>City Explorer</h1>
+      <h3>Where would you like to go?</h3>
         <form onSubmit={this.handleSubmit}>
           <input type="text" ref={this.textInput} />
           <button type="submit">Explore!</button>
         </form>
-        <h2>{this.state.cityName}</h2>
-        <h3>{this.state.lat}, {this.state.lon}</h3>
-      </Jumbotron>
+        <Jumbotron>
+          <h2>{this.state.cityName}</h2>
+          <h3>{this.state.lat}, {this.state.lon}</h3>
+          <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=10`} alt={this.state.cityName} />
+        </Jumbotron>
+      </>
     )
   }
 }
