8b3221f9231a60fa3ba61a8d35df92ebe8804c81
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-04-23 17:36:50 -0500

    moving along on movies... Constructor function and calling in props Movie component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Search from './Search';
 import Weather from './Weather';
 import Error from './Error';
+import Movies from './Movies';
 import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,6 +14,7 @@ class App extends React.Component {
       weather: '',
       city: '',
       haveSearched: false,
+      movies: [],
     }
   }
 
@@ -25,9 +27,9 @@ class App extends React.Component {
         cityName: locationURL.data[0].display_name,
         lat: locationURL.data[0].lat,
         lon: locationURL.data[0].lon,
-  
+
       })
-    } catch (error){
+    } catch (error) {
       this.setState({
         error: true,
         errorResponse: error.response.data.error,
@@ -35,39 +37,55 @@ class App extends React.Component {
       })
     }
     this.getWeather();
+    this.getMovies();
   }
 
-    getWeather = async () => {
-      try {
-        const cityWeather = await axios.get(`http://localhost:3002/weather`, {params: {lat: this.state.lat, lon: this.state.lon}});
-        console.log('Getting Weather Data', cityWeather);
-        this.setState({
-          weather: cityWeather.data
-        })
-      } catch (error){
-        this.setState({
-          error: true,
-          // errorResponse: error.response.data.error,
-          errorMessage: error.message
-        })
-      }
+  getWeather = async () => {
+    try {
+      const cityWeather = await axios.get(`http://localhost:3002/weather`, { params: { lat: this.state.lat, lon: this.state.lon } });
+      console.log('Getting Weather Data', cityWeather);
+      this.setState({
+        weather: cityWeather.data
+      })
+    } catch (error) {
+      this.setState({
+        error: true,
+        // errorResponse: error.response.data.error,
+        errorMessage: error.message,
+      })
     }
+  }
 
+  getMovies = async () => {
+    try {
+      const movieData = await axios.get(`http://localhost:3002/movies`, { params: { city: this.state.city}});
+      console.log('Getting Movie Data', movieData);
+      this.setState({
+        movies: movies.data,
 
+      })
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message,
+      })
+    }
+  }
 
   render() {
     console.log('This is the updated state of Weather', this.state.weather);
     return (
       <>
-        <Search 
+        <Search
           getLatLong={this.getLatLong}
-          cityName={this.state.cityName} 
-          lat={this.state.lat} 
-          lon={this.state.lon} 
+          cityName={this.state.cityName}
+          lat={this.state.lat}
+          lon={this.state.lon}
           haveSearched={this.state.haveSearched}
         />
         {this.state.error ? <Error errorMessage={this.state.errorMessage} /> : ''}
         <Weather weatherResults={this.state.weather} />
+        <Movies movies={this.state.movies} />
       </>
     )
   }
